af22743bf7813513f01de0dcf54fd954e4bcdb4c
Jawad Raza <jawadraza8372@gmail.com>
2023-04-13 11:43:04 +0500

    changing errors

--- server.js
+++ server.js
@@ -4,41 +4,41 @@ const express = require("express");
 const app = express();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-08-01",
+	apiVersion: "2022-08-01",
 });
-app.get("/",(req,res)=>{
-  res.send("Server Running")
-})
+app.get("/", (req, res) => {
+	res.send("Server Running");
+});
 
 app.get("/config", (req, res) => {
-  res.send({
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  });
+	res.send({
+		publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+	});
 });
 
 app.post("/create-payment-intent", async (req, res) => {
-  try {
-const { price } = req.body;
+	try {
+		const { price } = req.body;
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      currency: "EUR",
-      amount: price,
-      automatic_payment_methods: { enabled: true },
-    });
+		const paymentIntent = await stripe.paymentIntents.create({
+			currency: "EUR",
+			amount: price,
+			automatic_payment_methods: { enabled: true },
+		});
 
-    // Send publishable key and PaymentIntent details to client
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  } catch (e) {
-    return res.status(400).send({
-      error: {
-        message: e.message,
-      },
-    });
-  }
+		// Send publishable key and PaymentIntent details to client
+		res.send({
+			clientSecret: paymentIntent.client_secret,
+		});
+	} catch (e) {
+		return res.status(400).send({
+			error: {
+				message: e.message,
+			},
+		});
+	}
 });
-
-app.listen(5252, () =>
-  console.log(`Node server listening at http://localhost:5252`)
+const ports = process.env.PORT || 4000;
+app.listen(ports, () =>
+	console.log(`Node server listening at http://localhost:${ports}`)
 );
