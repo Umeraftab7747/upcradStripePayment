cda29b893b277e6120d2545507551cd3597cf3c9
Umer Aftab <67795704+Umeraftab7747@users.noreply.github.com>
2023-05-25 18:18:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
 	apiVersion: "2022-08-01",
 });
-app.use(cors());
+app.use(cors({
+"*"
+}));
 app.use(bodyParser.json({ limit: "200mb" }));
 app.use(bodyParser.urlencoded({ limit: "200mb", extended: true }));
 app.get("/", (req, res) => {
